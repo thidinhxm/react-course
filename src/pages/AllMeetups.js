eadd14c6624fc5d38740e4d7f615a7b2bc6337e7
thidinhxm <thidinhxm@gmail.com>
2022-01-21 20:26:55 +0700

    Adding more components

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,6 +1,39 @@
+const DUMMY_DATA = [
+    {
+      id: 'm1',
+      title: 'This is a first meetup',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      address: 'Meetupstreet 5, 12345 Meetup City',
+      description:
+        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+    },
+    {
+      id: 'm2',
+      title: 'This is a second meetup',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      address: 'Meetupstreet 5, 12345 Meetup City',
+      description:
+        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+    },
+  ];
+
 function AllMeetupsPage() {
     return (
-        <div>All Meetups Page</div>
+        <section>
+            <h1>All Meetups Page</h1>
+            <ul>
+                {DUMMY_DATA.map(meetup => (
+                    <li key={meetup.id}>
+                        <h2>{meetup.title}</h2>
+                        <img src={meetup.image} alt={meetup.title} />
+                        <p>{meetup.description}</p>
+                        <p>{meetup.address}</p>
+                    </li>
+                ))}
+            </ul>
+        </section>
     )
 }
 
