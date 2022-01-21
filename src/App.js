ba61e10a0ab782753e8ccc504f0e42b7cde5d591
thidinhxm <thidinhxm@gmail.com>
2022-01-21 21:00:14 +0700

    props children

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 
+import Layout from './components/layout/Layout';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
