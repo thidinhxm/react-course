d10059be67f40ed445ff2ff76f4bfd49731edf0e
thidinhxm <thidinhxm@gmail.com>
2022-01-22 10:51:07 +0700

    fetch api, useEffect

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import { useRef  } from 'react';
 
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
-function NewMeetupForm() {
+function NewMeetupForm(props) {
     const titleInputRef = useRef();
     const imageInputRef = useRef();
     const descriptionInputRef = useRef();
@@ -19,7 +19,8 @@ function NewMeetupForm() {
             description,
             address
         };
-        console.log(meetupData);
+        
+        props.onAddMeetup(meetupData);
     }
     return (
         <Card>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,25 @@
+import {useNavigate} from 'react-router-dom'
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-course-827ab-default-rtdb.firebaseio.com/meetups.json', {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            navigate('/');
+        })
+    }
+
     return (
         <section>
             <h1>New Meetup Page</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     )
 }
